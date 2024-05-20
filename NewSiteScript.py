5479c062b1b47db1179757ba1c8ac76a8f342182
Chris Russell <cruse1977123@gmail.com>
2024-05-20 22:22:43 +0100

    Initial

---
+++ NewSiteScript.py
@@ -0,0 +1,24 @@
+from extras.scripts import *
+from django.utils.text import slugify
+from dcim.choices import DeviceStatusChoices, SiteStatusChoices
+from dcim.models import Device, DeviceRole, DeviceType, Site
+
+class NewSiteScript(Script):
+    class Meta:
+        name = "New Branch"
+        description = "Provision a new branch site"
+    site_name = StringVar(
+        description="Name of the new site"
+    )
+
+    def run(self, data, commit):
+
+        # Create the new site
+        site = Site(
+            name=data['site_name'],
+            slug=slugify(data['site_name']),
+            status=SiteStatusChoices.STATUS_PLANNED
+        )
+        site.save()
+        self.log_success(f"Created new site: {site}")
+
